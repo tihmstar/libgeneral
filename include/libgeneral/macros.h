1facab5dfa072befb1a925ce525271d3d2a1fb6a
tihmstar <tihmstar@gmail.com>
2019-06-11 15:12:09 +0200

    m

--- include/libgeneral/macros.h
+++ include/libgeneral/macros.h
@@ -9,6 +9,8 @@
 #ifndef macros_h
 #define macros_h
 
+#include <assert.h>
+
 #ifdef DEBUG //versioning
 #   define VERSION_COMMIT_COUNT "Debug"
 #   define VERSION_COMMIT_SHA "Build: " __DATE__ " " __TIME__
@@ -30,17 +32,28 @@
 // ---- functions ----
 
 // -- logging --
+#ifndef CUSTOM_LOGGING
 #define info(a ...) ({printf(a),printf("\n");})
 #define log(a ...) ({printf(a),printf("\n");})
 #define warning(a ...) ({printf("[WARNING] "), printf(a),printf("\n");})
 #define error(a ...) ({printf("[Error] "),printf(a),printf("\n");})
+#endif
 
+#define NOTIMPLEMENTED() __assert_rtn(__func__, __FILE__, __LINE__, "not implemented")
 
 #define safeFree(ptr) ({if (ptr) free(ptr),ptr=NULL;})
+#define safeFreeCustom(ptr,func) ({if (ptr) func(ptr),ptr=NULL;})
+
+#ifdef __cplusplus
+#   ifndef NO_EXCEPT_ASSURE
+#       define EXCEPT_ASSURE
+#   endif
+#endif
 
 
 // -- assure --
-#ifdef __cplusplus
+#ifdef EXCEPT_ASSURE
+#include "exception.hpp"
 //assure cpp
 #   define assure(cond) if ((cond) == 0) throw tihmstar::EXPECTIONNAME(__LINE__, "assure failed", __FILE__)
 #   define retassure(cond, err) if ((cond) == 0) throw tihmstar::EXPECTIONNAME(__LINE__,err,__FILE__)
@@ -57,15 +70,15 @@
 
 
 
-//more cpp
+// //more cpp assure
 #   ifndef EXPECTIONNAME
-#       error EXPECTIONNAME_NAME_NOT_SET
+#       define EXPECTIONNAME exception
 #   endif
 
 #else
 //assure c
 #   define assure(a) do{ if ((a) == 0){err=__LINE__; goto error;} }while(0)
-#   define retassure(retcode, a) do{ if ((a) == 0){err=retcode; goto error;} }while(0)
+#   define retassure(cond, errstr ...) do{ if ((cond) == 0){error(errstr); goto error;} }while(0)
 #   define reterror(estr ...) do{error(estr);err=__LINE__; goto error; }while(0)
 
 #endif
