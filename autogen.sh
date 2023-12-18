45e6f58678bb7682b29f545fde907a35de8cd2e3
Elcomsoft R&D <elcomsoft.rd@elcomsoft.com>
2023-12-18 13:38:13 +0100

    fix compilation on legacy linux

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-#cleanup cache for correct versioning when run multiple times
-rm -rf autom4te.cache
+olddir=`pwd`
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
 
-aclocal -I m4
-autoconf
-autoheader
-automake --add-missing
-autoreconf -i
-./configure "$@"
+(
+  cd "$srcdir"
+
+  #cleanup cache for correct versioning when run multiple times
+  rm -rf autom4te.cache
+
+  gprefix=`which glibtoolize 2>&1 >/dev/null`
+  if [ $? -eq 0 ]; then
+    glibtoolize --force
+  else
+    libtoolize --force
+  fi
+  aclocal -I m4
+  autoheader
+  automake --add-missing
+  autoconf
+
+  cd "$olddir"
+)
+
+if [ -z "$NOCONFIGURE" ]; then
+  $srcdir/configure "$@"
+fi
