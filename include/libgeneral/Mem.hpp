9d15c9e3b3a6e2dde814ea5c86bc988fe33be903
Elcomsoft R&D <elcomsoft.rd@elcomsoft.com>
2024-07-25 10:23:06 +0200

    performance optimization for resize

--- include/libgeneral/Mem.hpp
+++ include/libgeneral/Mem.hpp
@@ -12,11 +12,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#define LIBGENERAL_MEM_DO_INLINE inline __attribute__((always_inline))
+
 namespace tihmstar {
 
 class Mem{
     uint8_t *_mem;
     size_t _memSize;
+    size_t _allocatedSize;
 public:
     Mem(size_t size = 0);
     Mem(const Mem &obj); //copy constructor
@@ -29,9 +32,9 @@ public:
     Mem& operator=(Mem &&old) noexcept; //move assignment
 
 #pragma mark quick access members
-    inline __attribute__((always_inline)) uint8_t *data() noexcept{return _mem;}
-    inline __attribute__((always_inline)) const uint8_t *data() const noexcept{return _mem;}
-    inline __attribute__((always_inline)) size_t size() const noexcept{return _memSize;}
+    LIBGENERAL_MEM_DO_INLINE uint8_t *data() noexcept{return _mem;}
+    LIBGENERAL_MEM_DO_INLINE const uint8_t *data() const noexcept{return _mem;}
+    LIBGENERAL_MEM_DO_INLINE size_t size() const noexcept{return _memSize;}
     
 #pragma mark function members
     void resize(size_t size);
@@ -46,15 +49,21 @@ class Typed_Mem {
     Mem _mem;
 public:
     Typed_Mem() :_mem(sizeof(T)){}
+    Typed_Mem(const T val) noexcept :_mem(&val, sizeof(val)){} //copy from T
     Typed_Mem(const void *data, size_t size):_mem(data,size){}
     Typed_Mem(Mem &&old) noexcept :_mem(old){} //move assignment
-    inline __attribute__((always_inline)) operator const T*() const {return (const T*)_mem.data();}
-    inline __attribute__((always_inline)) operator T*() {return (T*)_mem.data();}
 
-    inline __attribute__((always_inline)) const T *operator->() const {return (const T*)_mem.data();}
-    inline __attribute__((always_inline)) T *operator->() {return (T*)_mem.data();}
-    inline __attribute__((always_inline)) Mem &mem() {return _mem;}
+    template <typename U>
+    Typed_Mem(Typed_Mem<U> &&old) noexcept :_mem(old.mem()){} //move assignment
+    LIBGENERAL_MEM_DO_INLINE operator const T*() const {return (const T*)_mem.data();}
+    LIBGENERAL_MEM_DO_INLINE operator T*() {return (T*)_mem.data();}
+    LIBGENERAL_MEM_DO_INLINE const T *operator->() const {return (const T*)_mem.data();}
+    LIBGENERAL_MEM_DO_INLINE T *operator->() {return (T*)_mem.data();}
+    LIBGENERAL_MEM_DO_INLINE Mem &mem() {return _mem;}
 };
+
+
+#undef LIBGENERAL_MEM_DO_INLINE
 
 };
 #endif /* Mem_hpp */
--- libgeneral/Mem.cpp
+++ libgeneral/Mem.cpp
@@ -13,17 +13,18 @@ using namespace tihmstar;
 
 #pragma mark class Mem
 Mem::Mem(size_t size)
-: _mem(NULL), _memSize(size)
+: _mem(NULL), _memSize(size), _allocatedSize(0)
 {
-    if (_memSize)
-        assure(_mem = (uint8_t*)calloc(1,_memSize));
+    if (_memSize){
+        assure(_mem = (uint8_t*)calloc(1,_allocatedSize = _memSize));
+    }
 }
 
 Mem::Mem(const Mem &obj)
-: _mem(NULL), _memSize(obj._memSize)
+: _mem(NULL), _memSize(obj._memSize), _allocatedSize(0)
 {
     if (_memSize){
-        assure(_mem = (uint8_t*)malloc(_memSize));
+        assure(_mem = (uint8_t*)malloc(_allocatedSize = _memSize));
         memcpy(_mem, obj._mem, _memSize);
     }
 }
@@ -31,30 +32,33 @@ Mem::Mem(const Mem &obj)
 Mem::Mem(Mem &&old) noexcept{
     _mem = old._mem; old._mem = NULL;
     _memSize = old._memSize; old._memSize = 0;
+    _allocatedSize = old._allocatedSize; old._allocatedSize = 0;
 }
 
 Mem::Mem(void *mem, size_t memSize) noexcept
-: _mem((uint8_t*)mem), _memSize(memSize)
+: _mem((uint8_t*)mem), _memSize(memSize), _allocatedSize(memSize)
 {
     //
 }
 
 Mem::Mem(const void *mem, size_t memSize)
+: _mem(NULL), _memSize(memSize), _allocatedSize(memSize)
 {
-    _memSize = memSize;
-    _mem = (uint8_t*)malloc(_memSize);
-    memcpy(_mem, mem, _memSize);
+    if (_memSize){
+        _mem = (uint8_t*)malloc(_allocatedSize = _memSize);
+        memcpy(_mem, mem, _memSize);
+    }
 }
 
 Mem::~Mem(){
-    safeFree(_mem);
+    safeFree(_mem); _memSize = 0; _allocatedSize = 0;
 }
 
 Mem& Mem::operator=(const Mem &obj){
     safeFree(_mem);
     _memSize = obj._memSize;
     if (_memSize){
-        assure(_mem = (uint8_t*)malloc(_memSize));
+        assure(_mem = (uint8_t*)malloc(_allocatedSize = _memSize));
         memcpy(_mem, obj._mem, _memSize);
     }
     return *this;
@@ -64,33 +68,68 @@ Mem& Mem::operator=(Mem &&old) noexcept{
     safeFree(_mem);
     _mem = old._mem; old._mem = NULL;
     _memSize = old._memSize; old._memSize = 0;
+    _allocatedSize = old._allocatedSize; old._allocatedSize = 0;
     return *this;
 }
 
 #pragma mark function members
 
 void Mem::resize(size_t size){
+    if (size == _memSize) return;
     if (size) {
         if (_memSize) {
-            assure(_mem = (uint8_t*)realloc(_mem, _memSize = size));
+            if (size > _memSize) {
+                //grow
+                if (size > _allocatedSize) {
+                    //actually grow, because our allocation is smaller than what is requested
+                    if (size > _allocatedSize*2) {
+                        /*
+                             User requested an allocation bigger than twice what we already have.
+                             No optimizazions here, just alloc what was requested
+                         */
+                        _allocatedSize = size;
+                    }else{
+                        /*
+                            User wants to increase allocation "just a little bit".
+                            We will double our current size to have enough room for further small increases
+                         */
+                        _allocatedSize *= 2;
+                    }
+                    assure(_mem = (uint8_t*)realloc(_mem, _allocatedSize));
+                }
+            }else{
+                //shrink
+                if (size < _allocatedSize/4) {
+                    /*
+                        Only shrink the allocation if the user needs significantly less space than what we have allocated
+                     */
+                    _allocatedSize /= 2;
+                    assure(_mem = (uint8_t*)realloc(_mem, _allocatedSize));
+                }
+            }
+            _memSize = size;
         }else{
-            assure(_mem = (uint8_t*)malloc(_memSize = size));
+            assure(_mem = (uint8_t*)malloc(_allocatedSize = _memSize = size));
         }
     }else{
-        safeFree(_mem); _memSize = 0;
+        safeFree(_mem); _memSize = 0; _allocatedSize = 0;
     }
 }
 
 void Mem::appendSize(size_t size){
-    size_t oldSize = _memSize;
-    resize(oldSize + size);
-    memset(&_mem[oldSize], 0, size);
+    if (size) {
+        size_t oldSize = _memSize;
+        resize(oldSize + size);
+        memset(&_mem[oldSize], 0, size);
+    }
 }
 
 void Mem::append(const void *mem, size_t size){
-    size_t oldSize = _memSize;
-    resize(oldSize + size);
-    memcpy(&_mem[oldSize], mem, size);
+    if (size) {
+        size_t oldSize = _memSize;
+        resize(oldSize + size);
+        memcpy(&_mem[oldSize], mem, size);
+    }
 }
 
 Mem Mem::copy() const{
@@ -102,4 +141,5 @@ Mem Mem::copy() const{
 void Mem::disown(){
     _mem = NULL;
     _memSize = 0;
+    _allocatedSize = 0;
 }
